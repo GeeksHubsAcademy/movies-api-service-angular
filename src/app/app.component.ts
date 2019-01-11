7681c54faa6662bd7ec38daa5d3f4cb5caf4b3ef
Juan <jupegarnica@gmail.com>
2019-01-11 13:29:40 +0100

    load trending movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,23 @@ import { LoggerService } from './logger.service';
 })
 export class AppComponent {
   imgDomain = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/';
-  data: Array<object> = [];
-  constructor(private api: MoviesService, private logger: LoggerService) {
-    api.popularMovies().then((result: any) => {
+  data: Array<object>;
+  constructor(private api: MoviesService, private logger: LoggerService) {}
+  loadPopularMovies() {
+    this.api.popularMovies().then((result: any) => {
       this.data = result.results;
-      logger.log('peliculas recibidas');
+      this.logger.log('peliculas recibidas');
     });
-    logger.log('peliculas pedidas');
+    this.logger.log('peliculas pedidas');
+  }
+  loadTrendingMovies() {
+    this.api.trendingMovies().then((result: any) => {
+      this.data = result.results;
+      this.logger.log('peliculas recibidas');
+    });
+    this.logger.log('peliculas pedidas');
+  }
+  clean() {
+    this.data = undefined;
   }
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -7,15 +7,24 @@ import { LoggerService } from './logger.service';
 })
 export class MoviesService {
   apiKey = '323112ea2281b9eb70f319f4df422c6b';
-  endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}`;
+  popularEndpoint = `https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}`;
+  trendingMoviesEndpoint = `https://api.themoviedb.org/3/trending/all/day?api_key=${this.apiKey}`;
   http: HttpClient;
   constructor(http: HttpClient, private logger: LoggerService) {
     this.http = http;
   }
   popularMovies() {
     return new Promise((resolve, reject) => {
-      this.http.get(this.endpoint).subscribe(result => {
-        this.logger.log('tenemos las pelis!');
+      this.http.get(this.popularEndpoint).subscribe(result => {
+        this.logger.log('tenemos las pelis populares!');
+        resolve(result);
+      });
+    });
+  }
+  trendingMovies() {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.trendingMoviesEndpoint).subscribe(result => {
+        this.logger.log('tenemos las pelis trending!');
         resolve(result);
       });
     });
